0b7e2a0623caeeb49d4894309b9df16b909f0bca
Fatemeh <F_s_ghasemi@yahoo.com>
2024-01-07 19:21:10 +0330

    addClassLibrry

---
+++ Repository/InterFace/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using SharpPro.Contracts;
+using SharpPro.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpPro.Repository.InterFace;
+
+public interface IProductRepository:IGenericRepository<Product>
+{
+}
